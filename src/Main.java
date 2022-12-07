22e7bd4031470814664391722db091dadba9c248
Vittoria Vassarotti <112390959+Veetss@users.noreply.github.com>
2022-12-07 11:56:25 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> divisorsOf52 = new ArrayList<>();
+        Collections.addAll(divisorsOf52, 1, 2, 4, 13, 26, 52);
+        List<Integer> randomNumbers = new ArrayList<>();
+        Collections.addAll(randomNumbers, 1, 60, 13, 12);
+
+        randomNumbers.addAll(divisorsOf52);
+        System.out.println("Numbers: " + randomNumbers + " - Number of items: " + randomNumbers.size());
+
+        int x = randomNumbers.indexOf(1);
+        randomNumbers.remove(x);
+        int y = randomNumbers.indexOf(13);
+        randomNumbers.remove(y);
+
+        System.out.println(randomNumbers);
+
+        Collections.sort(randomNumbers, Collections.reverseOrder());
+        System.out.println(randomNumbers);
     }
 }
